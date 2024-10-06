719d0abfd87f6e68fdef65203e84fb6d21c636c9
m4zeboy <moises775azevedo@gmail.com>
2024-10-06 15:12:29 -0300

    parte 2 - protocolo 2

--- 1.2pilha-de-protocolos-receptor/index.c
+++ 1.2pilha-de-protocolos-receptor/index.c
@@ -3,9 +3,18 @@
 
 typedef struct byte
 {
-  char data[8];
+  short data[8];
   short size;
 } Byte;
+
+int power(int x, int n)
+{
+  if (n == 0)
+  {
+    return 1;
+  }
+  return x * power(x, n - 1);
+}
 
 int receive_low_level_layer(Byte low__level_layer_data[80])
 {
@@ -25,7 +34,7 @@ int receive_low_level_layer(Byte low__level_layer_data[80])
     }
     else
     {
-      low__level_layer_data[j].data[k] = input[i];
+      low__level_layer_data[j].data[k] = input[i] - '0';
       k++;
     }
     i++;
@@ -34,10 +43,28 @@ int receive_low_level_layer(Byte low__level_layer_data[80])
   return j + 1;
 }
 
+void mid_level_layer(Byte input[80], int size, int output[80])
+{
+  int i, j, pow, sum;
+
+  for (i = 0; i < size; i++)
+  {
+    pow = 0;
+    sum = 0;
+    for (j = input[i].size - 1; j >= 0; j--)
+    {
+      sum += input[i].data[j] * power(2, pow);
+      pow++;
+    }
+    output[i] = sum;
+  }
+}
+
 int main(void)
 {
   int size, i, j;
   Byte low__level_layer_data[80];
+  int mid__level_layer_data[80];
 
   for (i = 0; i < 80; i++)
   {
@@ -54,10 +81,19 @@ int main(void)
   {
     for (j = 0; j < low__level_layer_data[i].size; j++)
     {
-      printf("%c", low__level_layer_data[i].data[j]);
+      printf("%d", low__level_layer_data[i].data[j]);
     }
 
     printf(" ");
+  }
+
+  printf("\n");
+
+  mid_level_layer(low__level_layer_data, size, mid__level_layer_data);
+
+  for (i = 0; i < size; i++)
+  {
+    printf("%d ", mid__level_layer_data[i]);
   }
 
   return 0;
